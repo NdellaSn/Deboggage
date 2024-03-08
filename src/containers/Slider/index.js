bc803775487e32f62ce2da17cc06a59504835b5b
NdellaSn <ndellasene3@gmail.com>
2024-03-08 23:16:11 +0100

    rangement de eventment dans l'orde decroissant et affichage mois de l'evenement

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,8 +8,9 @@ const Slider = () => {
     const { data } = useData();
     const [index, setIndex] = useState(0);
     const byDateDesc = data?.focus.sort((evtA, evtB) =>
-        new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+        new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
     );
+
     const nextCard = () => {
         setTimeout(
             () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
